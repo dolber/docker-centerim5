8e1144fc352a2f64f81dcbaf2c60fdb562c62995
Alex <alex@our-dns.info>
2018-05-19 13:02:36 +0300

    Initial commit

---
+++ centerim5_alpine/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NOW="$(date +%F_%H.%M.%S)"
+
+if [ ! -d logs ]; then
+    mkdir -p logs
+fi
+
+if [ ! -d centerim5 ]; then
+    git clone -b mob git+ssh://mob@repo.or.cz/srv/git/centerim5.git
+fi
+
+# docker build --no-cache -t centerim5_alpine .  1>> logs/build_$NOW.txt 2>>logs/build_err_$NOW.txt
+docker build -t centerim5_alpine .  1>> logs/build_$NOW.txt 2>>logs/build_err_$NOW.txt
+
+
---
+++ centerim5_alpine/centerim5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [ ! -d $HOME/.centerim5 ]; then
+    mkdir -p $HOME/.centerim5
+fi
+
+
+docker run --rm -it \
+    -v $HOME/.centerim5:/root/.centerim5 \
+    centerim5_alpine:latest
